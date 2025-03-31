f5c5ad733521b35ecb75906f2244a96aa58d6a49
Torzhevskiy Iliya <Iliya.torzhevskiy@mail.ru>
2025-03-31 19:08:35 +0700

    Add Profile Page

--- vite-project-messenger/src/7_shared/Link/Link.ts
+++ vite-project-messenger/src/7_shared/Link/Link.ts
@@ -4,10 +4,18 @@ export const Link = `
 <a 
 href="{{href}}"
 {{#if (isSimpleEquals variant "text")}} 
-   class=${s["link_text"]}
+   {{#if (isSimpleEquals color "red")}} 
+   class="${`${s["link_text"]} ${s["link_red-text"]}`}"
+   {{else}}
+   class="${s["link_text"]}"
+   {{/if}}
 {{/if}}
 {{#if (isSimpleEquals variant "underline")}} 
+   {{#if (isSimpleEquals color "red")}} 
+   class="${`${s["link_underline"]} ${s["link_red-text"]}`}"
+   {{else}}
    class=${s["link_underline"]}
+   {{/if}}
 {{/if}}
 data-page="{{data-page}}">
 {{text}}
