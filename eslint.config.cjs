904aa726b468e0ad093b78d0cbdb019a5df6a470
Torzhevskiy Iliya <Iliya.torzhevskiy@mail.ru>
2025-05-05 01:25:08 +0700

    add eslintIgnore and edit readme

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,57 +1,56 @@
-const {
-    defineConfig,
-} = require("eslint/config");
+const { defineConfig } = require("eslint/config");
 
 const globals = require("globals");
 
-const {
-    fixupConfigRules,
-    fixupPluginRules,
-} = require("@eslint/compat");
+const { fixupConfigRules, fixupPluginRules } = require("@eslint/compat");
 
 const tsParser = require("@typescript-eslint/parser");
 const typescriptEslint = require("@typescript-eslint/eslint-plugin");
 const js = require("@eslint/js");
 
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
+const { FlatCompat } = require("@eslint/eslintrc");
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-module.exports = defineConfig([{
+module.exports = defineConfig([
+  {
     languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
+      globals: {
+        ...globals.browser,
+      },
 
-        parser: tsParser,
-        ecmaVersion: "latest",
-        sourceType: "module",
-        parserOptions: {},
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {},
     },
 
-    extends: fixupConfigRules(compat.extends(
+    extends: fixupConfigRules(
+      compat.extends(
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
-        "plugin:import/recommended",
-    )),
+        "plugin:import/recommended"
+      )
+    ),
 
     plugins: {
-        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+      "@typescript-eslint": fixupPluginRules(typescriptEslint),
     },
 
     rules: {
-        "no-console": 1,
-        "@typescript-eslint/no-non-null-assertion": 0,
-        "import/no-unresolved": 0,
-        "import/named": 2,
-        "import/namespace": 2,
-        "import/default": 2,
-        "import/export": 2,
+      "no-console": 1,
+      "@typescript-eslint/no-non-null-assertion": 0,
+      "import/no-unresolved": 0,
+      "import/named": 2,
+      "import/namespace": 2,
+      "import/default": 2,
+      "import/export": 2,
     },
-}]);
+
+    ignores: ["eslint.config.cjs", "server.js", "static/**"],
+  },
+]);
