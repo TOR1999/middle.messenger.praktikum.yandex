2a41dfbe2e8d43e525edc4527d4a22204f805976
Torzhevskiy Iliya <Iliya.torzhevskiy@mail.ru>
2025-05-11 23:05:14 +0700

    use block in in ProfilePageEditorPassword

--- src/3_pages/ProfilePageEditorPassword/ProfilePageEditorPassword.ts
+++ src/3_pages/ProfilePageEditorPassword/ProfilePageEditorPassword.ts
@@ -1,14 +1,16 @@
+import { Button } from "../../7_shared/Button/Button";
+import { CircleIconButton } from "../../7_shared/CircleIconButton/CircleIconButton";
+import { Input } from "../../7_shared/Input/Input";
+import { Typography } from "../../7_shared/Typography/Typography";
+import { Block } from "../../8_utils/helpers/block";
 import { getLang } from "../../8_utils/langs/getLang";
 import s from "./ProfilePageEditorPassword.module.scss";
 
-export const ProfilePageEditorPassword = `
+const profilePageEditorPasswordTemplate = `
 <div class=${s["container"]}>
   <div class=${s["button-back-container"]}>
     <div class=${s["button-back"]}>
-      {{> CircleIconButton 
-       iconSrc="/icons/arrowBack.svg"
-       altText="${getLang("common.buttons.altBack")}"
-       }}
+      {{{CircleIconButtonArrowBack}}}
     </div>
   </div>
   <form class=${s["content"]}>
@@ -19,57 +21,93 @@ export const ProfilePageEditorPassword = `
       />
     </div>
     <div class=${s["info-line-container"]}>
-      {{> Typography
-          variant="h3"
-          text="${getLang("profilePage.editPassword.old")}"
-       }}
+      {{{TypographyOldPassword}}}
       <div class=${s["info"]}>
-        {{> Input
-            inputId="oldPasswordId"
-            classStyle="textRight"
-            nameInput="oldPassword"
-            variant="password"
-            value=valueOldPassword
-        }}
+        {{{InputOldPassword}}}
       </div>
     </div>
     <div class=${s["info-line-container"]}>
-      {{> Typography
-          variant="h3"
-          text="${getLang("profilePage.editPassword.new")}"
-       }}
+      {{{TypographyNewPassword}}}
       <div class=${s["info"]}>
-        {{> Input
-            inputId="newPasswordId"
-            classStyle="textRight"
-            nameInput="newPassword"
-            variant="password"
-            value=valueNewPassword
-        }}
+        {{{InputNewPassword}}}
       </div>
     </div>
     <div class=${s["info-line-container"]}>
-      {{> Typography
-          variant="h3"
-          text="${getLang("profilePage.editPassword.repeatNew")}"
-       }}
+      {{{TypographyRepeatNewPassword}}}
       <div class=${s["info"]}>
-        {{> Input
-            inputId="repeatNewPasswordId"
-            classStyle="textRight"
-            nameInput="repeatNewPassword"
-            variant="password"
-            value=valueRepeatNewPassword
-        }}
+        {{{InputRepeatNewPassword}}}
       </div>
     </div>
    
     <div class=${s["button-save"]}>
-      {{> Button
-          id="editProfileId"
-          text="${getLang("common.buttons.save")}"
-      }}
+      {{{ButtonSaveNewPassword}}}
     </div>
   </form>
 </div>
 `;
+
+type TProps = {
+  valueOldPassword: string;
+  valueNewPassword: string;
+  valueRepeatNewPassword: string;
+};
+
+export class ProfilePageEditorPassword extends Block {
+  constructor(props: TProps) {
+    super("div", {
+      CircleIconButtonArrowBack: new CircleIconButton({
+        id: "arrowBackId",
+        iconSrc: "/icons/arrowBack.svg",
+        altText: getLang("common.buttons.altBack"),
+      }),
+      TypographyOldPassword: new Typography({
+        variant: "h3",
+        text: getLang("profilePage.editPassword.old"),
+      }),
+      InputOldPassword: new Input({
+        inputId: "oldPasswordId",
+        classStyle: "textRight",
+        nameInput: "oldPassword",
+        variant: "password",
+        value: props.valueOldPassword,
+      }),
+      TypographyNewPassword: new Typography({
+        variant: "h3",
+        text: getLang("profilePage.editPassword.new"),
+      }),
+      InputNewPassword: new Input({
+        inputId: "newPasswordId",
+        classStyle: "textRight",
+        nameInput: "newPassword",
+        variant: "password",
+        value: props.valueNewPassword,
+      }),
+      TypographyRepeatNewPassword: new Typography({
+        variant: "h3",
+        text: getLang("profilePage.editPassword.repeatNew"),
+      }),
+      InputRepeatNewPassword: new Input({
+        inputId: "repeatNewPasswordId",
+        classStyle: "textRight",
+        nameInput: "repeatNewPassword",
+        variant: "password",
+        value: props.valueRepeatNewPassword,
+      }),
+      ButtonSaveNewPassword: new Button({
+        id: "editProfileId",
+        text: getLang("common.buttons.save"),
+        disabled: false,
+        onClick: () => {
+          console.log({
+            oldPassword: props.valueOldPassword,
+            newPassword: props.valueNewPassword,
+          });
+        },
+      }),
+    });
+  }
+
+  override render() {
+    return this.compile(profilePageEditorPasswordTemplate, this.props);
+  }
+}
