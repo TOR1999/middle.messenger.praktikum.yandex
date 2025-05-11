bb777d3cfb0d5912b88a34dea455a88c71fd1b17
Torzhevskiy Iliya <Iliya.torzhevskiy@mail.ru>
2025-05-11 22:43:49 +0700

    use block in in ProfilePageEditorInfo

--- src/3_pages/ProfilePageEditorInfo/ProfilePageEditorInfo.ts
+++ src/3_pages/ProfilePageEditorInfo/ProfilePageEditorInfo.ts
@@ -1,14 +1,16 @@
+import { Button } from "../../7_shared/Button/Button";
+import { CircleIconButton } from "../../7_shared/CircleIconButton/CircleIconButton";
+import { Input } from "../../7_shared/Input/Input";
+import { Typography } from "../../7_shared/Typography/Typography";
+import { Block } from "../../8_utils/helpers/block";
 import { getLang } from "../../8_utils/langs/getLang";
 import s from "./ProfilePageEditorInfo.module.scss";
 
-export const ProfilePageEditorInfo = `
+const profilePageEditorInfoTemplate = `
 <div class=${s["container"]}>
   <div class=${s["button-back-container"]}>
     <div class=${s["button-back"]}>
-      {{> CircleIconButton 
-       iconSrc="/icons/arrowBack.svg"
-       altText="${getLang("common.buttons.altBack")}"
-       }}
+      {{{CircleIconButtonArrowBack}}}
     </div>
   </div>
   <form class=${s["content"]}>
@@ -19,101 +21,156 @@ export const ProfilePageEditorInfo = `
       />
     </div>
     <div class=${s["info-line-container"]}>
-      {{> Typography
-          variant="h3"
-          text="${getLang("profilePage.email")}"
-       }}
+      {{{TypographyEmail}}}
       <div class=${s["info"]}>
-        {{> Input
-            inputId="emailId"
-            classStyle="textRight"
-            nameInput="email"
-            variant="text"
-            textPlaceholder=valueEmail
-        }}
+        {{{InputEmail}}}
       </div>
     </div>
     <div class=${s["info-line-container"]}>
-      {{> Typography
-          variant="h3"
-          text="${getLang("common.login")}"
-       }}
+      {{{TypographyLogin}}}
       <div class=${s["info"]}>
-        {{> Input
-            inputId="loginId"
-            classStyle="textRight"
-            nameInput="login"
-            variant="text"
-            textPlaceholder=valueLogin
-        }}
+        {{{InputLogin}}}
       </div>
     </div>
     <div class=${s["info-line-container"]}>
-      {{> Typography
-          variant="h3"
-          text="${getLang("profilePage.name")}"
-       }}
+      {{{TypographyUserName}}}
       <div class=${s["info"]}>
-        {{> Input
-            inputId="firstNameId"
-            classStyle="textRight"
-            nameInput="first_name"
-            variant="text"
-            textPlaceholder=valueFirstName
-        }}
+        {{{InputUserName}}}
       </div>
     </div>
     <div class=${s["info-line-container"]}>
-      {{> Typography
-          variant="h3"
-          text="${getLang("profilePage.secondName")}"
-       }}
+      {{{TypographySecondName}}}
       <div class=${s["info"]}>
-        {{> Input
-            inputId="secondNameId"
-            classStyle="textRight"
-            nameInput="second_name"
-            variant="text"
-            textPlaceholder=valueSecondName
-        }}
+        {{{InputSecondName}}}
       </div>
     </div>
     <div class=${s["info-line-container"]}>
-      {{> Typography
-          variant="h3"
-          text="${getLang("profilePage.nickName")}"
-       }}
+      {{{TypographyNickName}}}
       <div class=${s["info"]}>
-        {{> Input
-            inputId="displayNameId"
-            classStyle="textRight"
-            nameInput="display_name"
-            variant="text"
-            textPlaceholder=valueNickName
-        }}
+        {{{InputNickName}}}
       </div>
     </div>
     <div class=${s["info-line-container"]}>
-      {{> Typography
-          variant="h3"
-          text="${getLang("profilePage.phone")}"
-       }}
+      {{{TypographyPhone}}}
       <div class=${s["info"]}>
-        {{> Input
-            inputId="phoneId"
-            classStyle="textRight"
-            nameInput="phone"
-            variant="text"
-            textPlaceholder=valuePhone
-        }}
+        {{{InputPhone}}}
       </div>
     </div>
     <div class=${s["button-save"]}>
-      {{> Button
-          id="editProfileId"
-          text="${getLang("common.buttons.save")}"
-      }}
+      {{{ButtonSaveInfoProfile}}}
     </div>
   </form>
 </div>
 `;
+
+type TProps = {
+  valueEmail: string;
+  valueLogin: string;
+  valueFirstName: string;
+  valueSecondName: string;
+  valueNickName: string;
+  valuePhone: string;
+};
+
+export class ProfilePageEditorInfo extends Block {
+  constructor(props: TProps) {
+    super("div", {
+      CircleIconButtonArrowBack: new CircleIconButton({
+        id: "arrowBackId",
+        iconSrc: "/icons/arrowBack.svg",
+        altText: getLang("common.buttons.altBack"),
+      }),
+      TypographyEmail: new Typography({
+        variant: "h3",
+        text: getLang("profilePage.email"),
+      }),
+      InputEmail: new Input({
+        value: "",
+        inputId: "emailId",
+        classStyle: "textRight",
+        nameInput: "email",
+        variant: "text",
+        textPlaceholder: props.valueEmail,
+      }),
+      TypographyLogin: new Typography({
+        variant: "h3",
+        text: getLang("common.login"),
+      }),
+      InputLogin: new Input({
+        value: "",
+        inputId: "loginId",
+        classStyle: "textRight",
+        nameInput: "login",
+        variant: "text",
+        textPlaceholder: props.valueLogin,
+      }),
+      TypographyUserName: new Typography({
+        variant: "h3",
+        text: getLang("profilePage.name"),
+      }),
+      InputUserName: new Input({
+        value: "",
+        inputId: "firstNameId",
+        classStyle: "textRight",
+        nameInput: "first_name",
+        variant: "text",
+        textPlaceholder: props.valueFirstName,
+      }),
+      TypographySecondName: new Typography({
+        variant: "h3",
+        text: getLang("profilePage.secondName"),
+      }),
+      InputSecondName: new Input({
+        value: "",
+        inputId: "secondNameId",
+        classStyle: "textRight",
+        nameInput: "second_name",
+        variant: "text",
+        textPlaceholder: props.valueSecondName,
+      }),
+      TypographyNickName: new Typography({
+        variant: "h3",
+        text: getLang("profilePage.nickName"),
+      }),
+      InputNickName: new Input({
+        value: "",
+        inputId: "displayNameId",
+        classStyle: "textRight",
+        nameInput: "display_name",
+        variant: "text",
+        textPlaceholder: props.valueNickName,
+      }),
+      TypographyPhone: new Typography({
+        variant: "h3",
+        text: getLang("profilePage.phone"),
+      }),
+      InputPhone: new Input({
+        value: "",
+        inputId: "phoneId",
+        classStyle: "textRight",
+        nameInput: "phone",
+        variant: "text",
+        textPlaceholder: props.valuePhone,
+      }),
+      ButtonSaveInfoProfile: new Button({
+        id: "buttonSaveProfile",
+        text: getLang("common.buttons.save"),
+        disabled: false,
+        onClick: () => {
+          console.log({
+            first_name: props.valueFirstName,
+            second_name: props.valueSecondName,
+            display_name: props.valueNickName,
+            login: props.valueLogin,
+            email: props.valueEmail,
+            phone: props.valuePhone,
+          });
+        },
+      }),
+    });
+  }
+
+  override render() {
+    return this.compile(profilePageEditorInfoTemplate, this.props);
+  }
+}
