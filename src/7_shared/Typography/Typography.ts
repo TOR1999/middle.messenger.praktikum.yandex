7151add33fe0aabf526634ff80443e860c99b4f8
Torzhevskiy Iliya <Iliya.torzhevskiy@mail.ru>
2025-05-12 00:31:01 +0700

    add props color in Typography

--- src/7_shared/Typography/Typography.ts
+++ src/7_shared/Typography/Typography.ts
@@ -3,46 +3,102 @@ import s from "./Typography.module.scss";
 const typographyTemplate = `
 <div
 {{#if (isSimpleEquals variant "b1")}} 
+   {{#if (isSimpleEquals color "red")}} 
+   class="${`${s["font-b1"]} ${s["red-text"]}`}"
+   {{else}}
    class=${s["font-b1"]}
+   {{/if}}
 {{/if}}
 {{#if (isSimpleEquals variant "b2")}} 
+   {{#if (isSimpleEquals color "red")}} 
+   class="${`${s["font-b2"]} ${s["red-text"]}`}"
+   {{else}}
    class=${s["font-b2"]}
+   {{/if}}
 {{/if}}
 {{#if (isSimpleEquals variant "b3")}} 
+   {{#if (isSimpleEquals color "red")}} 
+   class="${`${s["font-b3"]} ${s["red-text"]}`}"
+   {{else}}
    class=${s["font-b3"]}
+   {{/if}}
 {{/if}}
 {{#if (isSimpleEquals variant "b4")}} 
+   {{#if (isSimpleEquals color "red")}} 
+   class="${`${s["font-b4"]} ${s["red-text"]}`}"
+   {{else}}
    class=${s["font-b4"]}
+   {{/if}}
 {{/if}}
 {{#if (isSimpleEquals variant "b5")}} 
+   {{#if (isSimpleEquals color "red")}} 
+   class="${`${s["font-b5"]} ${s["red-text"]}`}"
+   {{else}}
    class=${s["font-b5"]}
+   {{/if}}
 {{/if}}
 {{#if (isSimpleEquals variant "b6")}} 
+   {{#if (isSimpleEquals color "red")}} 
+   class="${`${s["font-b6"]} ${s["red-text"]}`}"
+   {{else}}
    class=${s["font-b6"]}
+   {{/if}}
 {{/if}}
 {{#if (isSimpleEquals variant "b7")}} 
+   {{#if (isSimpleEquals color "red")}} 
+   class="${`${s["font-b7"]} ${s["red-text"]}`}"
+   {{else}}
    class=${s["font-b7"]}
+   {{/if}}
 {{/if}}
 {{#if (isSimpleEquals variant "h1")}} 
+ {{#if (isSimpleEquals color "red")}} 
+   class="${`${s["font-h1"]} ${s["red-text"]}`}"
+   {{else}}
    class=${s["font-h1"]}
+   {{/if}}
 {{/if}}
 {{#if (isSimpleEquals variant "h2")}} 
+ {{#if (isSimpleEquals color "red")}} 
+   class="${`${s["font-h2"]} ${s["red-text"]}`}"
+   {{else}}
    class=${s["font-h2"]}
+   {{/if}}
 {{/if}}
-{{#if (isSimpleEquals variant "h3")}} 
+{{#if (isSimpleEquals variant "h3")}}    
+   {{#if (isSimpleEquals color "red")}} 
+   class="${`${s["font-h3"]} ${s["red-text"]}`}"
+   {{else}}
    class=${s["font-h3"]}
+   {{/if}}
 {{/if}}
 {{#if (isSimpleEquals variant "h4")}} 
+ {{#if (isSimpleEquals color "red")}} 
+   class="${`${s["font-h4"]} ${s["red-text"]}`}"
+   {{else}}
    class=${s["font-h4"]}
+   {{/if}}
 {{/if}}
 {{#if (isSimpleEquals variant "h5")}} 
+ {{#if (isSimpleEquals color "red")}} 
+   class="${`${s["font-h5"]} ${s["red-text"]}`}"
+   {{else}}
    class=${s["font-h5"]}
+   {{/if}}
 {{/if}}
 {{#if (isSimpleEquals variant "h6")}} 
+ {{#if (isSimpleEquals color "red")}} 
+   class="${`${s["font-h6"]} ${s["red-text"]}`}"
+   {{else}}
    class=${s["font-h6"]}
+   {{/if}}
 {{/if}}
 {{#if (isSimpleEquals variant "h7")}} 
+ {{#if (isSimpleEquals color "red")}} 
+   class="${`${s["font-h7"]} ${s["red-text"]}`}"
+   {{else}}
    class=${s["font-h7"]}
+   {{/if}}
 {{/if}}
 >
 {{text}}
@@ -66,6 +122,7 @@ type TProps = {
     | "h6"
     | "h7";
   text: string;
+  color?: "red";
 };
 
 export class Typography extends Block {
