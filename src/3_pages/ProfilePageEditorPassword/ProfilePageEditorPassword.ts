6d70848c411e1ed446f279a3b7623e9476725d41
Torzhevskiy Iliya <Iliya.torzhevskiy@mail.ru>
2025-05-12 09:33:50 +0700

    add submit validate on ProfilePageEditorPassword

--- src/3_pages/ProfilePageEditorPassword/ProfilePageEditorPassword.ts
+++ src/3_pages/ProfilePageEditorPassword/ProfilePageEditorPassword.ts
@@ -3,6 +3,7 @@ import { CircleIconButton } from "../../7_shared/CircleIconButton/CircleIconButt
 import { Input } from "../../7_shared/Input/Input";
 import { Typography } from "../../7_shared/Typography/Typography";
 import { Block } from "../../8_utils/helpers/block";
+import { validatePassword } from "../../8_utils/helpers/validatePassword";
 import { getLang } from "../../8_utils/langs/getLang";
 import s from "./ProfilePageEditorPassword.module.scss";
 
@@ -24,18 +25,21 @@ const profilePageEditorPasswordTemplate = `
       {{{TypographyOldPassword}}}
       <div class=${s["info"]}>
         {{{InputOldPassword}}}
+        {{{TypographyOldPasswordError}}}
       </div>
     </div>
     <div class=${s["info-line-container"]}>
       {{{TypographyNewPassword}}}
       <div class=${s["info"]}>
         {{{InputNewPassword}}}
+        {{{TypographyNewPasswordError}}}
       </div>
     </div>
     <div class=${s["info-line-container"]}>
       {{{TypographyRepeatNewPassword}}}
       <div class=${s["info"]}>
         {{{InputRepeatNewPassword}}}
+        {{{TypographyRepeatNewPasswordError}}}
       </div>
     </div>
    
@@ -50,10 +54,28 @@ type TProps = {
   valueOldPassword: string;
   valueNewPassword: string;
   valueRepeatNewPassword: string;
+  onClick?: (e: Event) => void;
 };
 
 export class ProfilePageEditorPassword extends Block {
+  textError = false;
+
   constructor(props: TProps) {
+    const TypographyOldPasswordError = new Typography({
+      variant: "b7",
+      text: "",
+      color: "red",
+    });
+    const TypographyNewPasswordError = new Typography({
+      variant: "b7",
+      text: "",
+      color: "red",
+    });
+    const TypographyRepeatNewPasswordError = new Typography({
+      variant: "b7",
+      text: "",
+      color: "red",
+    });
     super("div", {
       CircleIconButtonArrowBack: new CircleIconButton({
         id: "arrowBackId",
@@ -68,7 +90,7 @@ export class ProfilePageEditorPassword extends Block {
         inputId: "oldPasswordId",
         classStyle: "textRight",
         nameInput: "oldPassword",
-        variant: "password",
+        variant: "text",
         value: props.valueOldPassword,
       }),
       TypographyNewPassword: new Typography({
@@ -79,7 +101,7 @@ export class ProfilePageEditorPassword extends Block {
         inputId: "newPasswordId",
         classStyle: "textRight",
         nameInput: "newPassword",
-        variant: "password",
+        variant: "text",
         value: props.valueNewPassword,
       }),
       TypographyRepeatNewPassword: new Typography({
@@ -90,14 +112,68 @@ export class ProfilePageEditorPassword extends Block {
         inputId: "repeatNewPasswordId",
         classStyle: "textRight",
         nameInput: "repeatNewPassword",
-        variant: "password",
+        variant: "text",
         value: props.valueRepeatNewPassword,
+        onBlur: (e: Event) => console.log("e input", e),
       }),
+      TypographyOldPasswordError,
+      TypographyNewPasswordError,
+      TypographyRepeatNewPasswordError,
       ButtonSaveNewPassword: new Button({
         id: "editProfileId",
         text: getLang("common.buttons.save"),
         disabled: false,
-        onClick: () => {
+        onClick: (e: Event) => {
+          e.preventDefault();
+
+          const inputOldPassword = document.getElementById(
+            "oldPasswordId",
+          ) as HTMLInputElement;
+          const oldPassword = inputOldPassword?.value;
+
+          const inputNewPassword = document.getElementById(
+            "newPasswordId",
+          ) as HTMLInputElement;
+          const newPassword = inputNewPassword?.value;
+
+          const inputRepeatNewPassword = document.getElementById(
+            "repeatNewPasswordId",
+          ) as HTMLInputElement;
+          const repeatNewPassword = inputRepeatNewPassword?.value;
+
+          if (oldPassword !== props.valueOldPassword) {
+            TypographyOldPasswordError.setProps({
+              text: getLang("validateText.oldPassword"),
+            });
+          } else {
+            TypographyOldPasswordError.setProps({ text: "" });
+          }
+
+          if (newPassword !== repeatNewPassword) {
+            TypographyRepeatNewPasswordError.setProps({
+              text: getLang("validateText.repeatPassword"),
+            });
+            TypographyNewPasswordError.setProps({
+              text: getLang("validateText.repeatPassword"),
+            });
+          } else {
+            if (validatePassword(newPassword)) {
+              TypographyRepeatNewPasswordError.setProps({
+                text: "",
+              });
+              TypographyNewPasswordError.setProps({
+                text: "",
+              });
+            } else {
+              TypographyRepeatNewPasswordError.setProps({
+                text: getLang("validateText.password"),
+              });
+              TypographyNewPasswordError.setProps({
+                text: getLang("validateText.password"),
+              });
+            }
+          }
+
           console.log({
             oldPassword: props.valueOldPassword,
             newPassword: props.valueNewPassword,
