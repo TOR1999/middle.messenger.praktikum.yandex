3c13d53f4e27acfc2b30361ae83b644bdf8b0277
Torzhevskiy Iliya <Iliya.torzhevskiy@mail.ru>
2025-05-17 20:38:24 +0700

    add message component

---
+++ src/7_shared/Message/Message.ts
@@ -0,0 +1,47 @@
+import { TMessage } from "../../1_app/types";
+import { Block } from "../../8_utils/helpers/block";
+import s from "./Message.module.scss";
+import { Typography } from "../Typography/Typography";
+
+const messageTemplate = (props: TProps) => {
+  const messagePosition = props.myMessage ? "" : "container_message-right";
+  const messageStyle = props.myMessage ? "message_other" : "message_my";
+
+  return `
+<div class="${`${s["container"]} ${s[messagePosition]}`}" id=${`${props.messageId}`}>
+  <div class="${`${s["message"]} ${s[messageStyle]}`}">
+    {{{TypographyText}}}
+    <div class="${s["time"]}">
+      {{{TypographyTime}}}
+  </div>
+  </div>
+</div>
+`;
+};
+
+export type TProps = {
+  messageId: string;
+  message: TMessage;
+  myMessage: boolean;
+  onClick?: (e: Event) => void;
+};
+
+export class Message extends Block {
+  constructor(props: TProps) {
+    super("div", {
+      ...props,
+      TypographyText: new Typography({
+        variant: "b5",
+        text: props.message.text,
+      }),
+      TypographyTime: new Typography({
+        variant: "b7",
+        text: props.message.timeSend,
+      }),
+    });
+  }
+
+  override render() {
+    return this.compile(messageTemplate(this.props as TProps), this.props);
+  }
+}
