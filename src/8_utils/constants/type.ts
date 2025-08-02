a5ec6daea4870795e3bb0842d6957344d8b35674
Torzhevskiy Iliya <Iliya.torzhevskiy@mail.ru>
2025-08-02 15:25:08 +0700

    add URL_NAMES

--- src/8_utils/constants/type.ts
+++ src/8_utils/constants/type.ts
@@ -22,3 +22,16 @@ export type TOptions = {
 export type TBlockProps = Record<string, unknown> & {
   attr?: Record<string, string>;
 };
+
+export type TProps = Record<string, unknown>;
+
+export const URL_NAMES = {
+  SIGNIN: "/",
+  SIGNUP: "/sign-up",
+  SETTINGS: "/settings",
+  MESSAGER: "/messenger",
+  EDIT_SETTINGS: "/edit-settings",
+  EDIT_PASSWORD: "/edit-password",
+  NOT_FOUND: "/not-found",
+  SERVER_ERROR: "/server-error",
+};
