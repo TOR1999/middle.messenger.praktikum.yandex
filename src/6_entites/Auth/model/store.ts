7f84f3728290e5d4ab6fe563a666c078925c0b75
Torzhevskiy Iliya <Iliya.torzhevskiy@mail.ru>
2025-08-03 23:20:17 +0700

    fix initialState in AuthStore

--- src/6_entites/Auth/model/store.ts
+++ src/6_entites/Auth/model/store.ts
@@ -1,5 +1,14 @@
 import { Store } from "../../../8_utils/helpers/store";
 
-const initialState = { "1": "fail" };
+const initialState = {
+  id: 0,
+  first_name: "",
+  second_name: "",
+  display_name: "",
+  login: "",
+  email: "",
+  phone: "",
+  avatar: "",
+};
 
 export const AuthStore = new Store(initialState);
