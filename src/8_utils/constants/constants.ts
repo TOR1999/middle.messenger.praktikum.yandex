bbcd4780461b7fe73d546642ac05d70e34632c06
Torzhevskiy Iliya <Iliya.torzhevskiy@mail.ru>
2025-08-06 00:21:25 +0700

    add BASE_URLS in constants

--- src/8_utils/constants/constants.ts
+++ src/8_utils/constants/constants.ts
@@ -1,1 +1,7 @@
 export const STORAGE_IS_AUTH = "isAuth";
+
+export const BASE_URLS = {
+  API: "https://ya-praktikum.tech/api/v2",
+  RESOURCES: "https://ya-praktikum.tech/api/v2/resources",
+  WSS: "wss://ya-praktikum.tech/ws/chats",
+};
