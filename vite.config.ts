4f096f8baf2357af26dd5f5389da301872bf3cff
Torzhevskiy Iliya <Iliya.torzhevskiy@mail.ru>
2025-08-07 01:13:48 +0700

    fix viteConfig

--- vite.config.ts
+++ vite.config.ts
@@ -2,33 +2,10 @@ import { defineConfig } from "vite";
 import handlebars from "vite-plugin-handlebars";
 import Unfonts from "unplugin-fonts/vite";
 
-export default defineConfig({
+const defaultConfig = {
   publicDir: "public",
   build: {
     outDir: "static",
-  },
-  server: {
-    host: "localhost",
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "https://ya-praktikum.tech",
-        changeOrigin: true,
-        configure: (proxy) => {
-          proxy.on("proxyRes", (proxyRes) => {
-            const cookies = proxyRes.headers["set-cookie"];
-            if (cookies) {
-              proxyRes.headers["set-cookie"] = cookies.map((cookie) =>
-                cookie
-                  .replace(/;\s*Secure/i, "")
-                  .replace(/;\s*SameSite=None/i, "")
-                  .replace(/domain=[^;]+/i, "domain=localhost"),
-              );
-            }
-          });
-        },
-      },
-    },
   },
   plugins: [
     handlebars(),
@@ -47,4 +24,37 @@ export default defineConfig({
       },
     }),
   ],
+};
+
+export default defineConfig(({ mode, command }) => {
+  if (command === "serve" && mode === "development") {
+    return {
+      ...defaultConfig,
+      server: {
+        host: "localhost",
+        port: 3000,
+        proxy: {
+          "/api": {
+            target: "https://ya-praktikum.tech",
+            changeOrigin: true,
+            configure: (proxy) => {
+              proxy.on("proxyRes", (proxyRes) => {
+                const cookies = proxyRes.headers["set-cookie"];
+                if (cookies) {
+                  proxyRes.headers["set-cookie"] = cookies.map((cookie) =>
+                    cookie
+                      .replace(/;\s*Secure/i, "")
+                      .replace(/;\s*SameSite=None/i, "")
+                      .replace(/domain=[^;]+/i, "domain=localhost"),
+                  );
+                }
+              });
+            },
+          },
+        },
+      },
+    };
+  }
+
+  return defaultConfig;
 });
